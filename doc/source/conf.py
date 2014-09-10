d084bcf4952d001a39883888557d8689fda44cd6
Doug Hellmann <doug@doughellmann.com>
2014-09-10 14:32:28 -0400

    Add RSS feed
    Publish an RSS feed of the changes to the specs repository to make the
    specs more discoverable.
    
    Change-Id: Ib4f84efab8b3af3ddd47d38d63a91362612bd655

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -30,8 +30,13 @@ extensions = ['sphinx.ext.autodoc',
               'sphinx.ext.intersphinx',
               'sphinx.ext.todo',
               'sphinx.ext.viewcode',
-              'oslosphinx'
+              'oslosphinx',
+              'yasfb',
              ]
+
+# Feed configuration for yasfb
+feed_base_url = 'http://specs.openstack.org/openstack/ironic-specs'
+feed_author = 'OpenStack Ironic Team'
 
 todo_include_todos = True
 
