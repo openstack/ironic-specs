94c8b8ce62e407b739a3f6654e019499b58debcf
David Shrewsbury <shrewsbury.dave@gmail.com>
2014-10-03 09:13:06 -0400

    Remove incubated theme
    Kilo is open, so remove the incubated HTML theme.
    
    Change-Id: Ia7de2569122a09604d93e09ed71fc5184663f891

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -105,7 +105,7 @@ html_theme = 'nature'
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-html_theme_options = {'incubating': True}
+#html_theme_options = {'incubating': True}
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
